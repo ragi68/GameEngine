14130b23780e1c538d7fba32fe7857f2ce502c13
ragi68 <62191560+ragi68@users.noreply.github.com>
2023-05-09 22:49:06 -0700

    Add files via upload

---
+++ Prototype/premake5.lua
@@ -0,0 +1,97 @@
+workspace "GameEngineProto"
+	architecture "x64"
+	configurations{"Debug", "Release", "Dist"} --debug, release, and distribution
+
+outputDirectory = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" --sets output directory extension dynamically. Format is {Configuration}-{System(OS)}-{Architectire(x32/x64)}
+project "GameEnginePrototype"
+	location "GameEnginePrototype"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" .. outputDirectory .. "/%{prj.name}") --puts all targeted files into the given output directory
+	objdir("bin-int/" .. outputDirectory .. "/%{prj.name}") --sets path of all object/intermediate files
+
+	files{
+		"%{prj.name}/src/**.h", --takes all files in src with .h 
+		"%{prj.name}/src/**.cpp" --takes all files with .cpp
+	}
+
+	includedirs{
+		"%{prj.name}/externalLibs/spdlogging/include" --include directories in project building 
+	}
+	
+
+	--only set for windows system (maybe mac/linux?)
+	filter "system:windows"
+		cppdialect "C++17" --setes c++ dialect
+		staticruntime "On" --sets runtimeLibrary to MultiThreaded and to allow DLL use
+		systemversion "latest" --sets SDK version to latest
+
+		defines{
+			"PROTO_PLATFORM_WINDOWS",
+			"PROTO_BUILD_DLL"
+		}
+
+		postbuildcommands{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputDirectory .. "/Prototype") --post-build command copies DLL file to the EXE Prototype file
+		}
+	--filters to define macros for different configurations
+	filter "configurations:Debug"
+		defines "DEBUG_APP"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RELEASE_PROTO"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DISTRIBUTION_VERSION"
+		optimize "On"
+
+	--later multithread support??
+
+project "Prototype"
+	location "Prototype"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputDirectory .. "/%{prj.name}") --puts all targeted files into the given output directory
+	objdir("bin-int/" .. outputDirectory .. "/%{prj.name}") --sets path of all object/intermediate files
+
+	files{
+		"%{prj.name}/src/**.h", --takes all files in src with .h 
+		"%{prj.name}/src/**.cpp" --takes all files with .cpp
+	}
+	--include directories in project building 
+	includedirs{
+		"GameEnginePrototype/externalLibs/spdlogging/include",
+		"GameEnginePrototype/src"
+	}
+
+	links{
+		"GameEnginePrototype"
+	}
+	
+
+	--only set for windows system (maybe mac/linux?)
+	filter "system:windows"
+		cppdialect "C++17" --setes c++ dialect
+		staticruntime "On" --sets runtimeLibrary to MultiThreaded and to allow DLL use
+		systemversion "latest" --sets SDK version to latest
+
+		defines{
+			"PROTO_PLATFORM_WINDOWS"
+		}
+
+	--filters to define macros for different configurations
+	filter "configurations:Debug"
+		defines "DEBUG_APP"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "RELEASE_PROTO"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DISTRIBUTION_VERSION"
+		optimize "On"
