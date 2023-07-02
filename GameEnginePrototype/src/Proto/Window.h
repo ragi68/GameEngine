6deaa8a3ca838ccb8db8cfa6abc496501e1af397
ragi68 <62191560+ragi68@users.noreply.github.com>
2023-07-02 14:08:19 -0700

    Update Window.h

--- GameEnginePrototype/src/Proto/Window.h
+++ GameEnginePrototype/src/Proto/Window.h
@@ -16,23 +16,23 @@ namespace Proto {
 	class PROTO_API AbstractWin { //create abstract window -> size and name(fixed??), window tick update, rendering with openGL, window vsync to maximize framerate??
 	public:
 
-		using EventCall = std::function<void(Events&)>; //creates 'function' of void that takes in event reference
-																											//|
-																											//|
-		//constructors.																						//|
-		AbstractWin() {}																					//|
-																											//|
-		virtual ~AbstractWin() {}																			//|
-																											//|
-		//is what it says																					//|
-		virtual void UpdateWindow() = 0;																    //|
-																											//|
-		//debug stuff																						//|
-		virtual unsigned int GetWidth() const = 0;															//|
-		virtual unsigned int GetHeight() const = 0;															//|
-																											//|
-																											//|
-		//gets events done to																				//|
+		using EventCall = std::function<void(Events&)>; //creates a 'function' of void that takes in event reference
+															//|
+															//|
+		//constructors.												//|
+		AbstractWin() {}											//|
+															//|
+		virtual ~AbstractWin() {}										//|
+															//|
+		//is what it says											//|
+		virtual void UpdateWindow() = 0;									//|
+															//|
+		//debug stuff												//|
+		virtual unsigned int GetWidth() const = 0;								//|
+		virtual unsigned int GetHeight() const = 0;								//|
+															//|
+															//|
+		//gets events done to											//|
 		virtual void EventCallBack(const EventCall& call) = 0; //makes a function to collect events dispatched to window layer. 
 		//sets vsync for window framerate optimization
 		virtual void VSync(bool enable) = 0;
